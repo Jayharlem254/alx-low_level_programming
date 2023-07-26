80486a3bae9274213f111c457381e4bb008ca405
Jayharlem254 <jothamkilimo321@gmail.com>
2023-07-26 10:34:35 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -12,13 +12,10 @@ int main(void)
 
     a[2] = 1024;
     n = sizeof(a) / sizeof(int);
+    int *p;
 
     /* Write your line of code here... */
-    /* This line modifies the value of a[2] without directly using the variable 'a'
-     * or modifying the variable 'p'.
-     */
-    int *p = &a[2];
-    *(p + 5) = 98;
+    *(p + 3) = 98;
 
     /* The code below will print: a[2] = 98 */
     printf("a[2] = %d\n", a[2]);
