aaed1801ddf07cce49f0e4d7358ecb19507b04cf
Jayharlem254 <jothamkilimo321@gmail.com>
2023-08-02 01:39:37 -0700

    task 4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - Calculates the factorial of a given number.
+ * @n: The number whose factorial is to be calculated.
+ *
+ * Return: Factorial of the number. -1 if n is negative (error).
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
+
