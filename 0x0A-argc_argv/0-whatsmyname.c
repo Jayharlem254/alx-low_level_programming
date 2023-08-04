1b32c45e50a4ba3e4d526e0d24a6e004d24432fe
Jayharlem254 <jothamkilimo321@gmail.com>
2023-08-04 00:09:15 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments (unused)
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* To avoid unused variable warning */
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
