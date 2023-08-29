e56fc621c20dc242617b0611b2806ddc31c7a56d
Jayharlem254 <jothamkilimo321@gmail.com>
2023-08-29 03:36:54 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,33 @@
-#include "lists.h"
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: A pointer to the head of the list.
- * Return: The number of nodes in the list.
+ * print_listint_safe - Prints a linked list
+ * @head: Pointer to the head of the linked list
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	const listint_t *current = head;
-	const listint_t *loop_node = NULL;
+	const listint_t *next;
 
 	while (current != NULL)
 	{
+		next = current->next;
+
 		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
 
-		if (current->next >= current)
+		if (next >= current)
 		{
-			loop_node = current;
+			printf("-> [%p] %d\n", (void *)next, next->n);
 			break;
 		}
 
-		current = current->next;
-	}
-
-	if (loop_node != NULL)
-	{
-		printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
-		exit(98);
+		current = next;
 	}
 
 	return (count);
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,32 +2,31 @@
 #include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t linked list safely.
+ * free_listint_safe - Frees a listint_t list.
  * @h: A pointer to a pointer to the head of the list.
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
 	listint_t *current = *h;
-	listint_t *next_node;
+	listint_t *temp;
+	size_t size = 0;
 
-	while (current != NULL)
+	while (current)
 	{
-		next_node = current->next;
-		free(current);
-		count++;
+		size++;
+		temp = current;
+		current = current->next;
 
-		if (next_node >= current)
+		if ((void *)temp <= (void *)current)
 		{
 			*h = NULL;
-			exit(98);
+			return (size);
 		}
-
-		current = next_node;
+		free(temp);
 	}
 
 	*h = NULL;
-	return (count);
+	return (size);
 }
 
